c068ebc13677b622c8fa521a854c7c514794cf7e
Mohamed Saleh <mohamedsaleh1984@hotmail.com>
2022-03-12 00:59:16 -0500

    n-th-tribonacci-number

--- dp.h
+++ dp.h
@@ -19,6 +19,23 @@ public:
 		}
 		return table[num];
 	}
+	//https://leetcode.com/problems/n-th-tribonacci-number
+	int tribonacci(int n) {
+		if (n == 0)
+			return 0;
+		if (n == 1)
+			return 1;
+		if (n == 2)
+			return 1;
+		vector<int>  table(n + 1);
+		table[0] = 0;
+		table[1] = 1;
+		table[2] = 1;
+		for (int i = 3; i <= n; i++) {
+			table[i] = table[i - 1] + table[i - 2] + table[i - 3];
+		}
+		return table[n];
+	}
 private:
 
 };
